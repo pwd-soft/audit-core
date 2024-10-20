39ed085e8f05620a880c5359c7ed2de3a277adac
Salman-PWD <nfs1310@gmail.com>
2024-10-20 17:06:53 +0600

    search for objections

--- src/PWD.Audit.Application.Contracts/FilterModel.cs
+++ src/PWD.Audit.Application.Contracts/FilterModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PWD.Audit.DtoModels;
+using PWD.Audit.Enum;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -13,5 +15,24 @@ namespace PWD.Audit
         public string SortBy { get; set; }
         public string SortOrder { get; set; }
         public bool IsDesc { get; set; }
+    }
+
+    public class ObjectionFilterModel : FilterModel 
+    {
+        public Guid? OfficeId { get; set; }
+        public string? FinancialYear { get; set; }
+        public ObjectionType? ObjectionType { get; set; }
+        public DirectorateType? DirectorateType { get; set; }
+        public bool IsBroadSheet { get; set; } = false;
+        public bool IsResolved { get; set; } = false;
+        //public bool IsActive { get; set; } = true;
+        //public DateTime Date { get; set; }
+        //public string Name { get; set; }
+        //public string Description { get; set; }
+        //public string Response { get; set; }
+        //public double Value { get; set; }
+        //public string Note { get; set; }
+        //public string Attachments { get; set; }
+        //public List<AssociateDto> Associates { get; set; }
     }
 }
--- src/PWD.Audit.Application/Services/ObjectionAppService.cs
+++ src/PWD.Audit.Application/Services/ObjectionAppService.cs
@@ -11,6 +11,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Volo.Abp.Application.Services;
 using Volo.Abp.Domain.Repositories;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
 
 namespace PWD.Audit.Services
 {
@@ -68,9 +69,41 @@ namespace PWD.Audit.Services
 
         public async Task<List<ObjectionDto>> GetListAsync() => ObjectMapper.Map<List<Objection>, List<ObjectionDto>>(await _repository.GetListAsync());
 
+        //to be removed, work with FilterObjections
         public async Task<List<ObjectionDto>> GetListByOfficeIdAsync(Guid officeId)
         {
             var objectionList = await _repository.GetListAsync(i => i.OfficeId == officeId);
+            return ObjectMapper.Map<List<Objection>, List<ObjectionDto>>(objectionList);
+        }
+
+        public async Task<List<ObjectionDto>> SearchObjections(ObjectionFilterModel filterCriteria)
+        {
+            var queryableList = await _repository.GetQueryableAsync();
+            //queryableList.Where(o => o.OfficeId == filterCriteria.OfficeId);
+
+            if (filterCriteria.OfficeId is not null)
+                queryableList.Where(o => o.OfficeId == filterCriteria.OfficeId);
+
+            if (filterCriteria.DirectorateType > 0)
+                queryableList.Where(o => o.DirectorateType == filterCriteria.DirectorateType);
+
+            if (filterCriteria.ObjectionType > 0)
+                queryableList.Where(o => o.ObjectionType == filterCriteria.ObjectionType);
+
+            if (filterCriteria.FinancialYear is not null)
+                queryableList.Where(o => o.FinancialYear == filterCriteria.FinancialYear);
+
+            if (filterCriteria.IsBroadSheet)
+                queryableList.Where(o => o.IsBroadSheet == filterCriteria.IsBroadSheet);
+
+            if (filterCriteria.IsResolved)
+                queryableList.Where(o => o.IsResolved == filterCriteria.IsResolved);
+
+            var objectionList = queryableList.ToList();
+            
+            //var objectionList = queryableList.Skip(filterCriteria.Offset)
+            //    .Take(filterCriteria.Limit).ToList();
+
             return ObjectMapper.Map<List<Objection>, List<ObjectionDto>>(objectionList);
         }
 
