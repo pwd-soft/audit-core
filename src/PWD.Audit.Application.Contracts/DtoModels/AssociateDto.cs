169270a1ebee77318ebfae222714f3e5614a3faa
Salman-PWD <nfs1310@gmail.com>
2024-10-21 16:53:59 +0600

    associatedto now inherits from FullAuditedEntityDto

--- src/PWD.Audit.Application.Contracts/DtoModels/AssociateDto.cs
+++ src/PWD.Audit.Application.Contracts/DtoModels/AssociateDto.cs
@@ -3,7 +3,7 @@ using Volo.Abp.Application.Dtos;
 
 namespace PWD.Audit.DtoModels
 {
-    public class AssociateDto : EntityDto<int>
+    public class AssociateDto : FullAuditedEntityDto<int>
     {
         public int ObjectionId { get; set; }
         public string Name { get; set; }
