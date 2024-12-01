5ad3fe940e0b38263a733c0766977de8902d0cb3
M. Nafees Salman Khan <salman.dev.pwd@hotmail.com>
2024-12-01 12:08:02 +0600

    changed objection type enum serial

--- src/PWD.Audit.Domain.Shared/Enum/ObjectionType.cs
+++ src/PWD.Audit.Domain.Shared/Enum/ObjectionType.cs
@@ -4,9 +4,9 @@ namespace PWD.Audit.Enum
     public enum ObjectionType
     {
         None = 0,
-        Draft = 1,
+        NonSFI = 1,
         SFI = 2,
-        NonSFI = 3,
+        Draft = 3,
     }
 
     public enum DirectorateType
