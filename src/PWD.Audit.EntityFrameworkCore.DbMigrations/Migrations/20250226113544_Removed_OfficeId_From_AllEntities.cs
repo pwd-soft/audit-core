00947a92f631959f0e20de4ad3a420372bcca752
M. Nafees Salman Khan <salman.dev.pwd@hotmail.com>
2025-02-26 17:36:23 +0600

    migration to remove office id from all entites

---
+++ src/PWD.Audit.EntityFrameworkCore.DbMigrations/Migrations/20250226113544_Removed_OfficeId_From_AllEntities.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PWD.Audit.Migrations
+{
+    public partial class Removed_OfficeId_From_AllEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OfficeId",
+                table: "YearlyObjections");
+
+            migrationBuilder.DropColumn(
+                name: "OfficeId",
+                table: "Summaries");
+
+            migrationBuilder.DropColumn(
+                name: "OfficeId",
+                table: "ResponseHistory");
+
+            migrationBuilder.DropColumn(
+                name: "OfficeId",
+                table: "OfficeUsers");
+
+            migrationBuilder.DropColumn(
+                name: "OfficeId",
+                table: "Objections");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "OfficeId",
+                table: "YearlyObjections",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "OfficeId",
+                table: "Summaries",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "OfficeId",
+                table: "ResponseHistory",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "OfficeId",
+                table: "OfficeUsers",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "OfficeId",
+                table: "Objections",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+    }
+}
