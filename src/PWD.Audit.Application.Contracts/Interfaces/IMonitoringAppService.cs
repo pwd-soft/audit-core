994fe6a1a3a41cd23231ca8ecbfc1eb5abcc6e38
Salman <salman.dev.pwd@gmail.com>
2026-08-09 12:08:08 +0600

    Add MonitoringAppService with response count method
    Added IMonitoringAppService interface and its implementation MonitoringAppService. Injected IObjectionAppService and provided an authorized GetResponseCountByOffice method to retrieve incoming response counts by office code.

---
+++ src/PWD.Audit.Application.Contracts/Interfaces/IMonitoringAppService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PWD.Audit.Interfaces
+{
+    public interface IMonitoringAppService
+    {
+
+    }
+}
---
+++ src/PWD.Audit.Application/Services/MonitoringAppService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using PWD.Audit.Entities;
+using PWD.Audit.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace PWD.Audit.Services
+{
+    [Authorize]
+    public class MonitoringAppService : ApplicationService, IMonitoringAppService
+    {
+        //private readonly IRepository<Objection, int> _objectionRepository;
+        private readonly IObjectionAppService _objectionService;
+
+//IRepository<Objection, int> objectionRepository,
+        public MonitoringAppService( IObjectionAppService objectionService)
+        {
+            //_objectionRepository = objectionRepository;
+            _objectionService = objectionService;
+        }
+
+        public async Task<int> GetResponseCountByOffice(string officeCode)
+        {
+            var latestResponses = await _objectionService.GetIncomingResponseListAsync(officeCode);
+            return latestResponses.Count;
+        }
+    }
+
+
+}
